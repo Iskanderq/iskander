cfc6868d4c30a9c99513d817ca0671d111dcb797
Iskanderq <144662867+Iskanderq@users.noreply.github.com>
2023-09-28 15:41:14 +0300

    Update main.c

--- main.c
+++ main.c
@@ -4,7 +4,7 @@ int main (int argo, char ++argv)
 {
 char first[255,] last [255];
 
-printf("Enter your first name: ");
+cout<<"Enter your first name: ";
 fgets(first, 255, stdin);
 first[strlen(first)-1]='\0'; /* remove the newline at the end*/
 printf("Now enter your last name: ");
