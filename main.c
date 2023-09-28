8da7787e01422210998900376b4d49a4958e67f8
Iskander <gasimov9898@gmail.com>
2023-09-28 15:28:10 +0300

    edit main.c

--- main.c
+++ main.c
@@ -1,6 +1,15 @@
-#include <iostream>
-
-int main()
+#include <stdio.h>
+#include <string.h>
+int main (int argo, char ++argv)
 {
-	return 0;
+char first[255,] last [255];
+
+printf("Enter your first name: ");
+fgets(first, 255, stdin);
+first[strlen(first)-1]='\0'; /* remove the newline at the end*/
+printf("Now enter your last name: ");
+gets(last);/*butter overflow? what's that?*/
+printf("Hello %s %s!\n", first, last);
+return 0;
+
 }
